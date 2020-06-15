16dce82062408043efe7eccc0cc78a2d6b11cdeb
Lucas Garron <code@garron.net>
2020-06-15 00:47:44 -0700

    Finish web implementation.

--- src/targets/web/web.ts
+++ src/targets/web/web.ts
@@ -1,12 +1,60 @@
-import { AuthStorage, info, parseRepo, setAuthStorage } from "../../index";
-import { addLogListener } from "../../log";
+import { addLogListener, create, info, parseRepo, RepoSpec } from "../..";
+import { deleteBranch, replace, set } from "../../cmd";
+
+enum LocalStorageKey {
+  PAT = "github-personal-access-token",
+  Repo = "main-branch-saved-repo"
+}
 
 window.addEventListener("DOMContentLoaded", async () => {
-
-  const console = document.body.querySelector("#console");
-  addLogListener((...args) => {
-    console.textContent += args.join(" ") + "\n";
+  const repoElem = document.body.querySelector("#repo") as HTMLInputElement;
+  if (LocalStorageKey.Repo in localStorage && !repoElem.value) {
+    repoElem.value = localStorage[LocalStorageKey.Repo];
+  }
+  repoElem.addEventListener("change", () => {
+    localStorage["main-branch-saved-repo"] = repoElem.value;
+  })
+  repoElem.addEventListener("keyup", () => {
+    localStorage["main-branch-saved-repo"] = repoElem.value;
   })
 
-  info(parseRepo("https://github.com/cubing/fmc-duel"))
-})
+  document.body.querySelector("#setPAT").addEventListener("click", () => {
+    localStorage[LocalStorageKey.PAT] = prompt("Enter a personal access token from GitHub");
+  });
+
+  document.body.querySelector("#clearPAT").addEventListener("click", () => {
+    delete localStorage[LocalStorageKey.PAT];
+    alert("PAT has been cleared from local storage");
+  });
+
+  const consoleElem = document.body.querySelector("#console");
+  addLogListener((...args) => {
+    consoleElem.textContent += args.join(" ") + "\n";
+    consoleElem.scrollTop = consoleElem.scrollHeight
+  })
+
+  function getRepo(): RepoSpec {
+    return parseRepo(repoElem.value);
+  }
+  
+  const commandsElem = document.querySelector("#commands");
+
+  function registerCommand(selector: string, fn): void {
+    const commandElem = commandsElem.querySelector(selector);
+    commandElem.addEventListener("click", () => {
+      const branch = (commandElem.parentElement.querySelector(".branch") as HTMLInputElement).value;
+      if (branch) {
+        fn(getRepo(), branch)
+      } else {
+        fn(getRepo())
+      }
+    })
+
+  }
+
+  registerCommand("#info", info);
+  registerCommand("#create", create);
+  registerCommand("#set", set);
+  registerCommand("#replace", replace);
+  registerCommand("#delete", deleteBranch);
+});
